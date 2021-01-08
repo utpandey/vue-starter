b17bb8fe4398ff6dd42d15cba2f35b756ff3de70
unknown <utsavpandey8@gmail.com>
2021-01-08 23:47:46 +0530

    app changes

---
+++ app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+<div id="app">
+  {{ message }} Nice to meet Vue.
+</div>
+</template>
+
+<script>
+
+
+const App = {
+  data() {
+    return {
+      message: 'This is a good place to type things.'  
+    }
+  }
+}
+
+export default {
+  App
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  margin-top: 60px;
+}
+</style>
+
